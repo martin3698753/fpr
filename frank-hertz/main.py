0d655a9fdebf93cd4721e41710710ddad1e6f0f0
martin3698753 <martinek8425@gmail.com>
2023-10-16 15:29:07 +0200

    script

--- frank-hertz/main.py
+++ frank-hertz/main.py
@@ -5,6 +5,9 @@ from scipy.signal import find_peaks, argrelextrema
 
 plt.rcParams['text.usetex'] = True
 
+neon_min = np.array([26.4, 44, 64.1])
+hg_min = np.array([28.6, 33.4, 38.3, 43.1, 47.9, 52.7, 57.5, 62.3, 68.1, 72.7, 77.7])
+
 def mes(name):
     mess = pd.read_csv(name + ".csv", index_col=0)
     mess['Input_A [V]'] = mess['Input_A [V]']*10
@@ -19,9 +22,26 @@ def mes(name):
     mess['min'] = mess.iloc[argrelextrema(mess['Input_B [V]'].values, np.less_equal,
                                           order=2)[0]]['Input_B [V]']
     plt.scatter(mess['Input_A [V]'], mess['min'], c='r')
+    plt.grid(axis='x', linestyle='--')
 
-    #plt.xticks(np.append(np.arange(0, 100, 10), mess['min']))
-    plt.show()
-    print(mess.to_markdown())
+    if(name == "neon"):
+        plt.xticks(np.append(np.arange(0, 80, 10), neon_min))
+        tab = neon_min
+    if(name == "hg"):
+        plt.xticks(np.append(np.array([0, 10, 20]), hg_min))
+        tab = hg_min
+    #print(mess.to_markdown())
+    #plt.show()
+    tab_min = np.zeros(tab.shape[0])
+    for i in range(tab.shape[0]-1):
+        tab_min[i+1] = tab[i+1] - tab[i]
+
+    df_min = pd.DataFrame(
+        {
+            "Ua":tab,
+            "delta Ua":tab_min
+        }
+    )
+    print(df_min)
 
 mes("neon")
