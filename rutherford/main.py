7ea120c025710e92d1a1914d6ae76bb346a1fc2f
martin3698753 <martinek8425@gmail.com>
2023-10-25 01:02:54 +0200

    rf

--- rutherford/main.py
+++ rutherford/main.py
@@ -10,13 +10,13 @@ from matplotlib.widgets import Button, Slider
 #at 30 deg it was 0,3 V
 #at 0 deg it was 0.8 V
 #
-
-
+plt.rcParams['text.usetex'] = True
 
 def graph(angl):
-    a = 1
-    b = 0
-    dw = np.sin((angl-b)/2)**4
+    angl = angl * (np.pi/180)
+    a = 0.00056
+    b = 0.055
+    dw = np.sin((angl-b)/(2))**4
     return (a/dw)
 
 t = np.concatenate((np.repeat(100, 7), np.repeat(200, 2), np.repeat(600, 2), np.repeat(900, 2)))
@@ -34,8 +34,15 @@ df = pd.DataFrame(
     }
 )
 df = df.round(3)
-print(df)
-plt.scatter(ang, N)
-plt.plot(angl, graph(angl))
+df.to_csv("data/data1.csv", index=False)
+#print(df)
+plt.scatter(ang, N, label="data")
+plt.plot(angl, graph(angl), c='green', label=r"$f(\theta)$")
 plt.ylim(0, 50)
-plt.show()
+plt.xlabel("úhel rozptylu " + r"$\theta$", fontsize=12)
+plt.ylabel("četnost částic " + r"$N(\theta)$", fontsize=12)
+plt.annotate(r"$A = 56 \cdot 10^{-6}$", xy=(-30, 40))
+plt.annotate(r"$B = 55 \cdot 10^{-3}$", xy=(-30, 38))
+plt.legend()
+plt.savefig("data/graph1.pdf")
+#plt.show()
