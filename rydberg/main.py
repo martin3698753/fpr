ad379a5cdf09375ffe15c421312b0d90571c79d8
martin3698753 <martinek8425@gmail.com>
2023-12-11 10:19:38 +0100

    balmer

--- rydberg/main.py
+++ rydberg/main.py
@@ -44,7 +44,10 @@ sod_df = pd.DataFrame(
         "mrizka e6":np.multiply(d, 10**6),
     }
 )
+sod_df = sod_df.round(3)
+sod_df.to_csv("tabs/sod.csv", index=False)
 #print(sod_df)
+#print("d mean: ", dk, "| d std: ", np.std(d, ddof=1))
 ##Hydrogen
 hyd_f = np.array([313])
 hyd_l = np.array([295.666, 290, 277.166])
@@ -58,24 +61,41 @@ for i in range(2,3):
     theta_h[i] = (hyd_r[i] - hyd_l[i])/2
     lam[i] = (dk * np.sin( np.deg2rad(theta_h[i]) ))/(2)
 rh = np.zeros(3)
-for i in range(2):
-    rh[i] = (1/(lam[i])) * ( (1/(2**2)) - (1/(3**2)) )**(-1)
-for i in range(2,3):
-    rh[i] = (1/(lam[i])) * ( (1/(2**2)) - (1/(4**2)) )**(-1)
 
-hyd_df = pd.DataFrame(
+col_df = pd.DataFrame(
     {
-        "theta":theta_h,
-        "lambda[nm]":np.multiply(lam, 10**9),
-        "rh":rh,
+        "n2":np.arange(3,8),
+        "lambda":np.array([656, 486, 434, 410, 397]),
+        "colors":np.array(["red", "teal", "blue", "indigo", "violet"])
     }
 )
+col_df.to_csv("tabs/colors.csv", index=False)
+
+rh[0] = (1/(lam[0])) * ( (1/(2**2)) - (1/(4**2)) )**(-1)
+rh[1] = (1/(lam[1])) * ( (1/(2**2)) - (1/(3**2)) )**(-1)
+rh[2] = (1/(lam[2])) * ( (1/(2**2)) - (1/(6**2)) )**(-1)
+
+colors = np.array(["modrozelená", "červená", "indigo"])
+dif = np.array([1, 1, 2])
+hyd_df = pd.DataFrame(
+    {
+        "doleva":hyd_l,
+        "doprava":hyd_r,
+        "theta":theta_h,
+        "difrakce":dif,
+        "lambda[nm]":np.multiply(lam, 10**9),
+        "color":colors,
+        "rh e6":np.multiply(rh, 10**(-6)),
+    }
+)
+hyd_df = hyd_df.round(3)
 mp = 1.672721777*10**(-27)
 me = 9.10938215*10**(-31)
 c = 299792458
 e0 = 8.8541878176*10**(-12)
 e = 1.602176634*10**(-19)
 rh_inf = np.mean(rh) * ((mp + me)/mp)
-print(rh_inf)
-print( ((me * e**4) / (8*e0**2 * rh_inf * c))**(1/3) )
+#print(rh_inf)
+#print( ((me * e**4) / (8*e0**2 * rh_inf * c))**(1/3) )
 print(hyd_df)
+print("rh_inf ", rh_inf)
