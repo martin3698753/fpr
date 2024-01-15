e8d5ee72d889dd77aff58f70d564471acb06d52c
martin3698753 <martinek8425@gmail.com>
2024-01-15 20:05:52 +0100

    milikan

---
+++ milikan/main.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+num = np.arange(1, 16, 1)
+voltage = np.array([323, 425, 424, 64, 112, 168, 173, 78, 116, 130, 150, 80, 36, 120, 42])
+time = np.array([11.384, 4.362, 1.588, 4.579, 21.303, 21.303, 2.128, 2.04, 6.738, 13.675, 9.521, 7.563, 11.318, 11.456, 18.594, 11.659])
+dilek = np.array([1, 3, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+
+# 1, 8, 13
+temp = np.array([21.6, 22, 22.1])
+RH = np.array([40, 40, 39])
+hPa = np.array([964.7, 965, 965.2])
