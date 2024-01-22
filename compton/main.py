40c1c3719f2e562ea1cec546a932e3e555601e4c
martin3698753 <martinek8425@gmail.com>
2024-01-22 21:16:34 +0100

    k

--- compton/main.py
+++ compton/main.py
@@ -2,9 +2,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("data/Co60.csv", comment='#')
-x = df[" Energy (keV)"]
-y = df[" Counts"]
-plt.xlim(800, 1600)
-plt.plot(x, y)
-plt.show()
+df = pd.read_csv("data/0stA.csv", comment='#')
+print(df)
+#x = df[" Energy (keV)"]
+#y = df[" Counts"]
+#plt.xlim(800, 1600)
+#plt.plot(x, y)
+#plt.show()
