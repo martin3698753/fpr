95607948e74026df0d19fd14ab997c6d257bd5e7
martin3698753 <martinek8425@gmail.com>
2024-02-07 16:07:56 +0100

    com

--- compton/main.py
+++ compton/main.py
@@ -14,6 +14,7 @@ from scipy.signal import find_peaks as fp
 
 angs = ['0', '10', '20', '30', '40', '50', '60', '70']
 energy = np.zeros(8)
+energy_c = np.zeros(8)
 
 fig, ax = plt.subplots(4, 2)
 fig.tight_layout()
@@ -25,24 +26,54 @@ def fun1(angle, vec, h, index):
     peaks = fp(y, height=h, distance=120)
     yp = peaks[1]['peak_heights']
     xp = x[peaks[0]].values
+    yp_c = yp
+    xp_c = xp
 
-    if (angle == '30' or angle == '40' or angle == '50' or angle == '60'):
+    if (angle == '20' or angle == '30' or angle == '40'):
         yp = yp[1]
         xp = xp[1]
+        yp_c = yp_c[2]
+        xp_c = xp_c[2]
+
+    if (angle == '50'):
+        yp = yp[2]
+        xp = xp[2]
+        yp_c = yp_c[4]
+        xp_c = xp_c[4]
+    if (angle == '60'):
+        yp = yp[6]
+        xp = xp[6]
+        yp_c = yp_c[9]
+        xp_c = xp_c[9]
+    if (angle == '70'):
+        yp = yp[5]
+        xp = xp[5]
+        yp_c = yp_c[8]
+        xp_c = xp_c[8]
     energy[index] = xp.item()
-    ax[vec].scatter(xp, yp)
+    energy_c[index] = xp_c.item()
+    ax[vec].scatter(xp, yp, c='blue')
+    ax[vec].scatter(xp_c, yp_c, c='red')
     ax[vec].plot(x, y)
     ax[vec].set_title("hello", fontsize=8)
 
 fun1(angs[0], (0,0), 10000, 0)
 fun1(angs[1], (0,1), 5000, 1)
-fun1(angs[2], (1,0), 1000, 2)
+fun1(angs[2], (1,0), 500, 2)
 fun1(angs[3], (1,1), 300, 3)
 fun1(angs[4], (2,0), 250, 4)
-fun1(angs[5], (2,1), 200, 5)
-fun1(angs[6], (3,0), 180, 6)
-fun1(angs[7], (3,1), 180, 7)
-print(energy)
+fun1(angs[5], (2,1), 150, 5)
+fun1(angs[6], (3,0), 50, 6)
+fun1(angs[7], (3,1), 40, 7)
 plt.savefig("figs/fig1.pdf")
 plt.show()
+plt.clf()
 
+df = pd.DataFrame(
+    {
+        "angle":angs,
+        "E":energy,
+        "E'":energy_c,
+    }
+)
+print(df)
